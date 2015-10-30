2d41abe9dd0c2a8c9d372fa476b3079fceb9ef22
James Snee <jamessnee1@gmail.com>
2015-10-30 15:58:19 +1100

    added directions API

--- app/src/main/java/group107/wifiapp/CreateHotspotActivity.java
+++ app/src/main/java/group107/wifiapp/CreateHotspotActivity.java
@@ -91,6 +91,7 @@ public class CreateHotspotActivity extends FragmentActivity {
     private LatLng start_fromPosition, start_toPosition;
     private LatLng end_fromPosition, end_toPosition;
     private Polyline mapLine;
+    private PolylineOptions lineOptions;
     private boolean polyline;
     int numOfUsersChoice = 0;
     int dataAllowedChoice = 0;
@@ -253,6 +254,14 @@ public class CreateHotspotActivity extends FragmentActivity {
                         numOfMarkers = 2;
                         drawPolyline();
 
+                        // Getting URL to the Google Directions API
+                        String url = getMapsApiDirectionsUrl(start, end, selectedMode);
+
+                        DownloadTask downloadTask = new DownloadTask();
+
+                        // Start downloading json data from Google Directions API
+                        downloadTask.execute(url);
+
                     }
 
 
@@ -310,6 +319,15 @@ public class CreateHotspotActivity extends FragmentActivity {
                         //draw polyline
                         if (numOfMarkers == 2) {
                             drawPolyline();
+
+                            // Redo line drawing
+                            String url = getMapsApiDirectionsUrl(start, end, selectedMode);
+
+                            DownloadTask downloadTask = new DownloadTask();
+
+                            // Start downloading json data from Google Directions API
+                            downloadTask.execute(url);
+
                         }
 
                     }
@@ -323,6 +341,14 @@ public class CreateHotspotActivity extends FragmentActivity {
                             endPointMarker.position(end_toPosition);
                             //draw polyline
                             drawPolyline();
+
+                            // Redo line drawing
+                            String url = getMapsApiDirectionsUrl(start, end, selectedMode);
+
+                            DownloadTask downloadTask = new DownloadTask();
+
+                            // Start downloading json data from Google Directions API
+                            downloadTask.execute(url);
                         }
 
                     }
@@ -375,8 +401,8 @@ public class CreateHotspotActivity extends FragmentActivity {
 
         if (polyline == false) {
             //We only want one polyline between two points
-            PolylineOptions options = new PolylineOptions().add(start,end).width(5).color(Color.RED);
-            mapLine = mMap.addPolyline(options);
+            //PolylineOptions options = new PolylineOptions().add(start,end).width(5).color(Color.RED);
+            //mapLine = mMap.addPolyline(options);
             polyline = true;
         }
         else {
@@ -833,6 +859,136 @@ public class CreateHotspotActivity extends FragmentActivity {
         return bestLocation;
     } //end getLastKnownLocation
 
+    /** A method to download json data from url */
+    private String downloadUrl(String strUrl) throws IOException{
+        String data = "";
+        InputStream iStream = null;
+        HttpURLConnection urlConnection = null;
+        try{
+            URL url = new URL(strUrl);
+
+            // Creating an http connection to communicate with url
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            // Connecting to url
+            urlConnection.connect();
+
+            // Reading data from url
+            iStream = urlConnection.getInputStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+
+            StringBuffer sb = new StringBuffer();
+
+            String line = "";
+            while( ( line = br.readLine()) != null){
+                sb.append(line);
+            }
+
+            data = sb.toString();
+
+            br.close();
+
+        }catch(Exception e){
+            Log.d("Exception while downloading url", e.toString());
+        }finally{
+            iStream.close();
+            urlConnection.disconnect();
+        }
+        return data;
+    }
+
+    /** A class to parse the Google Places in JSON format */
+    private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String,String>>> >{
+
+        // Parsing the data in non-ui thread
+        @Override
+        protected List<List<HashMap<String, String>>> doInBackground(String... jsonData) {
+
+            JSONObject jObject;
+            List<List<HashMap<String, String>>> routes = null;
+
+            try{
+                jObject = new JSONObject(jsonData[0]);
+                DirectionsJSONParser parser = new DirectionsJSONParser();
+
+                // Starts parsing data
+                routes = parser.parse(jObject);
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            return routes;
+        }
+
+        // Executes in UI thread, after the parsing process
+        @Override
+        protected void onPostExecute(List<List<HashMap<String, String>>> result) {
+            ArrayList<LatLng> points = null;
+            lineOptions = null;
+            MarkerOptions markerOptions = new MarkerOptions();
+
+            // Traversing through all the routes
+            for(int i=0;i<result.size();i++){
+                points = new ArrayList<LatLng>();
+                lineOptions = new PolylineOptions();
+
+                // Fetching i-th route
+                List<HashMap<String, String>> path = result.get(i);
+
+                // Fetching all the points in i-th route
+                for(int j=0;j<path.size();j++){
+                    HashMap<String,String> point = path.get(j);
+
+                    double lat = Double.parseDouble(point.get("lat"));
+                    double lng = Double.parseDouble(point.get("lng"));
+                    LatLng position = new LatLng(lat, lng);
+
+                    points.add(position);
+                }
+
+                // Adding all the points in the route to LineOptions
+                lineOptions.addAll(points);
+                lineOptions.width(5);
+                lineOptions.color(Color.RED);
+            }
+
+            // Drawing polyline in the Google Map for the i-th route
+            mapLine = mMap.addPolyline(lineOptions);
+        }
+    }
+
+    // Fetches data from url passed
+    private class DownloadTask extends AsyncTask<String, Void, String>{
+
+        // Downloading data in non-ui thread
+        @Override
+        protected String doInBackground(String... url) {
+
+            // For storing data from web service
+            String data = "";
+
+            try{
+                // Fetching the data from web service
+                data = downloadUrl(url[0]);
+            }catch(Exception e){
+                Log.d("Background Task",e.toString());
+            }
+            return data;
+        }
+
+        // Executes in UI thread, after the execution of
+        // doInBackground()
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            ParserTask parserTask = new ParserTask();
+
+            // Invokes the thread for parsing the JSON data
+            parserTask.execute(result);
+        }
+    }
+
     //get method
     public static String GET(String url) throws IOException {
 
@@ -865,7 +1021,6 @@ public class CreateHotspotActivity extends FragmentActivity {
         }
         return result;
     }
-
 
     //AsyncTask for GET Request
     private class GoogleMapsAsyncTask extends AsyncTask<String, Void, String> {
